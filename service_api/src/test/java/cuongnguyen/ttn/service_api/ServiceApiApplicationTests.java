c2ef91f3893195a63359597a234211efb227032c
offshore01 <cuong-nv@if-vietnam.com>
2021-04-29 11:30:44 +0700

    git_commit

---
+++ service_api/src/test/java/cuongnguyen/ttn/service_api/ServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cuongnguyen.ttn.service_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
